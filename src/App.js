111b9c74afb66d64db7e733084fd78483381782c
light_dev <dgr8steve95@gmail.com>
2023-09-24 01:12:32 +0100

    added content

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Content from "./components/Content";
 
 function App() {
   return (
-    <div className="container App">
+    <div className=" App">
       <Header />
       <Content />
       <Footer />
